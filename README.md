08ed8b0830ca065d4f059753c67b4d64430070b7
chairbender <kwhipke1@gmail.com>
2015-11-21 12:34:17 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## object-calisthenics-analyzer
+
+A tool for analyzing Java code for its adherence to [Object Calisthenics](http://www.cs.helsinki.fi/u/luontola/tdd-2009/ext/ObjectCalisthenics.pdf).
---
+++ src/main/java/com/chairbender/object_calisthenics_analyzer/ObjectCalisthenicsAnalyzer.java
@@ -0,0 +1,9 @@
+package com.chairbender.object_calisthenics_analyzer;
+
+/**
+ * Main class for the application. Lets one invoke the analyzer programatically.
+ *
+ * Created by chairbender on 11/21/2015.
+ */
+public class ObjectCalisthenicsAnalyzer {
+}
